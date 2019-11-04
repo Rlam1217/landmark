04d72f6bd68bb2e03a5e64c982f7156cc3dba5d5
rlam1217 <rlam1217@gmail.com>
2019-11-03 22:04:18 -0800

    adds smooth scroll & hides nav bar on scroll down

--- js/main.js
+++ js/main.js
@@ -30,3 +30,16 @@ function toggleMenu() {
         showMenu = false;
     }
 }
+
+// Hides Nav Bar on scroll down
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById('navbar').style.top = "0";
+  } else {
+    document.getElementById('navbar').style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
